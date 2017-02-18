2f7d50e418e8aedbd3592b7c5e94605e0460b9c7
tnj <kitakore@gmail.com>
2017-02-18 14:49:59 +0900

    altitude difference display via pressure sensor

--- app/src/main/java/sh/nothing/droidbike/MainActivity.java
+++ app/src/main/java/sh/nothing/droidbike/MainActivity.java
@@ -2,33 +2,33 @@ package sh.nothing.droidbike;
 
 import android.content.Context;
 import android.databinding.DataBindingUtil;
-import android.databinding.ViewDataBinding;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.util.AttributeSet;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.util.Log;
 import android.view.View;
+
+import java.util.List;
+import java.util.Locale;
 
 import sh.nothing.droidbike.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity implements SensorEventListener {
 
+    private static final String TAG = "MainActivity";
     private SensorManager sensorManager;
     private Sensor pressure;
     private Sensor temperature;
     private ActivityMainBinding binding;
 
-    float basePressure = 1013.25f;
-    float lastPressure = 1000.0f;
-    float lastTemperature = 15.0f;
+    float basePressure = 0.0f;
+    float lastPressure = 0.0f;
+    float lastRawPressure = 0.0f;
+    float lastTemperature = 25.0f;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,11 +36,10 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         setContentView(R.layout.activity_main);
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
 
-        setSupportActionBar(binding.toolbar);
         binding.fab.setOnClickListener(view -> {
-            basePressure = lastPressure;
+            basePressure = lastRawPressure;
             Snackbar
-                .make(view, "Calibrated: " + lastPressure, Snackbar.LENGTH_LONG)
+                .make(view, "Calibrated: " + lastRawPressure, Snackbar.LENGTH_LONG)
                 .setAction("Action", null)
                 .show();
         });
@@ -49,60 +48,67 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         // a particular sensor.
         sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
         pressure = sensorManager.getDefaultSensor(Sensor.TYPE_PRESSURE);
-        temperature = sensorManager.getDefaultSensor(Sensor.TYPE_AMBIENT_TEMPERATURE);
+        findTemperatureSensor();
+
+        hideSystemControls();
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+    private void hideSystemControls() {
+        View decor = this.getWindow().getDecorView();
+        decor.setSystemUiVisibility(
+            View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+    private Sensor findTemperatureSensor() {
+        List<Sensor> list = sensorManager.getSensorList(Sensor.TYPE_ALL);
+        list.forEach(sensor -> {
+            if ("BMP280 temperature".equals(sensor.getName())) {
+                temperature = sensor;
+                Log.v(TAG, sensor.getName());           // => BMP280 temperature
+                Log.v(TAG, sensor.getStringType());     // => com.google.sensor.internal_temperature
+                Log.v(TAG, "Type=" + sensor.getType()); // => Type=65536
+            }
+        });
+        return temperature;
     }
 
     @Override
     protected void onStart() {
         super.onStart();
-        sensorManager.registerListener(this, pressure, SensorManager.SENSOR_DELAY_NORMAL);
-        sensorManager.registerListener(this, temperature, SensorManager.SENSOR_DELAY_NORMAL);
+        sensorManager.registerListener(this, pressure, SensorManager.SENSOR_DELAY_UI);
+        if (temperature != null)
+            sensorManager.registerListener(this, temperature, SensorManager.SENSOR_DELAY_NORMAL);
     }
 
     @Override
     protected void onStop() {
         super.onStop();
-        sensorManager.unregisterListener(this);
+        sensorManager.unregisterListener(this, pressure);
+        if (temperature != null)
+            sensorManager.unregisterListener(this, temperature);
     }
 
     @Override
     public void onSensorChanged(SensorEvent event) {
-        switch(event.sensor.getType()) {
-            case Sensor.TYPE_PRESSURE:
-                lastPressure = event.values[0];
-                break;
-            case Sensor.TYPE_AMBIENT_TEMPERATURE:
-                lastTemperature = event.values[0];
-                break;
+        if (event.sensor == pressure) {
+            lastRawPressure = event.values[0];
+            if (lastPressure == 0.0f) lastPressure = lastRawPressure;
+            if (basePressure == 0.0f) basePressure = lastRawPressure;
+            lastPressure += (lastRawPressure - lastPressure) * 0.2;
+        } else if (event.sensor == temperature) {
+            lastTemperature = event.values[0];
         }
 
-        String text = Float.toString(lastPressure) + "hPa\n" +
-            Float.toString(lastTemperature) + "℃\n" +
-            calculateHeight(lastPressure, basePressure, lastTemperature) + "m";
+        binding.content.pressure.setText(String.format(Locale.US, "%.2f", lastPressure));
+        binding.content.temperature.setText(String.format(Locale.US, "%.2f", lastTemperature));
+        String height = String.format(Locale.US, "%.2f", calculateHeight(lastPressure, basePressure, lastTemperature));
+        int pointIndex = height.indexOf('.');
+        binding.content.height.setText(height.substring(0, pointIndex));
+        binding.content.heightSub.setText(height.substring(pointIndex));
 
-        binding.content.helloWorld.setText(text);
     }
 
     private float calculateHeight(float lastPressure, float basePressure, float lastTemperature) {
